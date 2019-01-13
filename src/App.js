1e1aa2220fc6f450b7ded8538d2a6752c07bf166
Chris Andreotti <chris.andreotti@gmail.com>
2019-01-13 14:49:09 -0500

    App setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            columnDefs: [
+                { headerName: "Make", field: "make", sortable:true, filter:strue },
+                { headerName: "Model", field: "model", sortable:true, filter:true },
+                { headerName: "Price", field: "price", sortable:true, filter:true }
+
+            ],
+            rowData: [
+                {make: "Toyota", model: "Celica", price: 35000},
+                {make: "Ford", model: "Mondeo", price: 32000},
+                {make: "Porsche", model: "Boxter", price: 72000}
+            ]
+        }
+    }
+
+    render() {
+        return (
+                <div 
+                  className="ag-theme-material"
+                  style={{ 
+	                height: '500px', 
+	                width: '600px' }} 
+		            >
+                    <AgGridReact
+                        columnDefs={this.state.columnDefs}
+                        rowData={this.state.rowData}>
+                    </AgGridReact>
+                </div>
+            );
+    }
 }
 
 export default App;
